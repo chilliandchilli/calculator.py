128076b3d5cd65ff44d5412853f1d1bbf32f2be6
chilliandchilli <35531705+chilliandchilli@users.noreply.github.com>
2018-02-10 12:50:29 +0100

    prvi

---
+++ calculator.py
@@ -0,0 +1,23 @@
+x = 5
+y = 7
+print x + y
+
+x = int(raw_input("Enter the value for x: "))
+print x
+
+y = int(raw_input("Enter the value for y: "))
+print y
+
+operation = raw_input("Choose math operation (+, -, *, /): ")
+print operation
+
+if operation == "+":
+    print x + y
+elif operation == "-":
+    print x - y
+elif operation == "*":
+    print x * y
+elif operation == "/":
+    print x / y
+else:
+    print "You did not provide the correct math operation."
